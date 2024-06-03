e7ab74cd0d0af0fef9ad98bf829f951fc11fa3ba
thebookcheese <jasperthebook@gmail.com>
2024-06-03 13:07:38 +0100

    Please Please work

---
+++ Made_Up_News/Factbook.js
@@ -0,0 +1,10 @@
+let count = 0
+
+function Astano() {
+    if (count > 1) {
+        document.getElementById("Astano").innerHTML = "Population: 102 million <p> GDP: $1.03 trillion</p> <p> <b> Army Stats </b></p><p> Soldiers: 562,910 </p><p> Reserves: 5,240,691 </p><p> Tanks: 2,340 </p><p> Artillary: 5,360 </p><p> <b> Navy Stats </b> </p><p> Destroyers: 40 </p><p> Up-To-Date Destroyers: 15</p><p> Frigates: 80</p><p> Up-To-Date Frigates: 52</p><p>Submarines: 310</p><p> Up-To-Date Submarines: 200"
+        count = count + 1
+    } else {
+        count = count + 1
+   }
+}
