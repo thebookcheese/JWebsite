4dac9f339f405b022256581fac126033e0ece35d
thebookcheese <jasperthebook@gmail.com>
2024-06-03 20:03:14 +0100

    Added 2nd Fun Button

--- Fun_Stuff/Fun.js
+++ Fun_Stuff/Fun.js
@@ -1,8 +1,14 @@
 function FunFunction1() {
     result = confirm("Are you the boss")
     if (result == true) {
-        window.location.href = "http://www.jasperweb.pages.dev/Fun_Stuff/Fun1.html";
+        window.location.href = "https://jasperweb.pages.dev/Fun_Stuff/Fun1";
     } else {
         alert("Not boss")
     }
+}
+
+function FunFunction2() {
+    str1 = prompt("Enter a string")
+    str2 = prompt("Enter a string you want conbined")
+    alert(str1 + str2)
 }
