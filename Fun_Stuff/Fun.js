ca09a9f52f76a8842cfc554f465fe3ed786f058a
thebookcheese <jasperthebook@gmail.com>
2024-06-03 19:54:14 +0100

    Added Fun stuff!

---
+++ Fun_Stuff/Fun.js
@@ -0,0 +1,8 @@
+function FunFunction1() {
+    result = confirm("Are you the boss")
+    if (result == true) {
+        window.location.href = "http://www.jasperweb.pages.dev/Fun_Stuff/Fun1.html";
+    } else {
+        alert("Not boss")
+    }
+}
