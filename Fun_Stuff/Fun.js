4d81f231692e870e3e633b731b8ff32aac349ce5
thebookcheese <jasperthebook@gmail.com>
2024-06-05 13:04:17 +0100

    Added Stuff

--- Fun_Stuff/Fun.js
+++ Fun_Stuff/Fun.js
@@ -1,14 +1,19 @@
 function FunFunction1() {
-    result = confirm("Are you the boss")
+    result = confirm("Are you the boss");
     if (result == true) {
         window.location.href = "https://jasperweb.pages.dev/Fun_Stuff/Fun1";
     } else {
-        alert("Not boss")
+        alert("Not boss");
     }
 }
 
 function FunFunction2() {
-    str1 = prompt("Enter a string")
-    str2 = prompt("Enter a string you want conbined")
-    alert(str1 + str2)
+    str1 = prompt("Enter a string");
+    str2 = prompt("Enter a string you want combined");
+    alert(str1 + str2);
+}
+
+function FunFunction3() {
+    Input = prompt("Enter a text");
+    document.getElementById("FunButton3-1").innerHTML = Input;
 }
