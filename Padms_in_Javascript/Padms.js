28472975724bdf60a3bb458c68fa40bc4c0be8f1
thebookcheese <jasperthebook@gmail.com>
2024-06-07 11:13:16 +0100

    I do want i want (Body inside head)

---
+++ Padms_in_Javascript/Padms.js
@@ -0,0 +1,8 @@
+function Travel() {
+    WhereTravel = prompt("Where do you want to go; \n Creepy Cave (1) \n (2) Peter House")
+    if (WhereTravel = "1") {
+        window.location.href = "https://jasperweb.pages.dev/Padms_in_Javascript/Locations/Creepy_Cave";
+    } else if (WhereTravel = "2") {
+        window.location.href = "https://jasperweb.pages.dev/Padms_in_Javascript/Locations/Peter_House";
+    }
+}
