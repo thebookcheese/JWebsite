1deefce3d82cd6df0a3cad0815c572d6ca22a93b
thebookcheese <jasperthebook@gmail.com>
2024-06-10 08:27:33 +0100

    AG

--- Padms_in_Javascript/Padms.js
+++ Padms_in_Javascript/Padms.js
@@ -1,8 +1,10 @@
 function Travel() {
     WhereTravel = prompt("Where do you want to go; \n Creepy Cave (1) \n (2) Peter House")
-    if (WhereTravel = "1") {
+    if (WhereTravel == "1") {
         window.location.href = "https://jasperweb.pages.dev/Padms_in_Javascript/Locations/Creepy_Cave";
-    } else if (WhereTravel = "2") {
+    } else if (WhereTravel == "2") {
         window.location.href = "https://jasperweb.pages.dev/Padms_in_Javascript/Locations/Peter_House";
+    } else {
+        alert("Not valid")
     }
 }
