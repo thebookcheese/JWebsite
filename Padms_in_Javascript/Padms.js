b0f165e403ce349163ab4ece0553e5781a93baab
thebookcheese <jasperthebook@gmail.com>
2024-06-11 13:26:49 +0100

    Added Basics of PH location and commented stuff

--- Padms_in_Javascript/Padms.js
+++ Padms_in_Javascript/Padms.js
@@ -1,6 +1,11 @@
-var Enemy ="";
-const PHouseEnemys = ["Peter Dad", "Peter"];
+var Enemy =""; // Define Enemy (Used in Locations to get the enemy) 
 
+// Define Player Stats and Attacks
+function Slash() {
+    alert("WIP")
+}
+
+// Town Functions
 function Travel() {
     var WhereTravel = prompt("Where do you want to go; \n Creepy Cave (1) \n Peter House (2)")
     if (WhereTravel == "1") {
@@ -12,7 +17,21 @@ function Travel() {
     }
 }
 
+
+// Peter House Location Functions
+const PHouseEnemys = ["Peter Dad", "Peter"];
 function PeterHouseEnemyGet() {
     Enemy = Math.floor((Math.random()) * 3 );
     document.getElementById("EnemyGet").innerHTML = "The enemy is" +PHouseEnemys[Enemy];
+}
+
+function PeterHouseFight() {
+    document.getElementById("PeterHouseRun").innerHTMl = <button onclick="Slash">Slash</button>;
+    const newButton = document.createElement('button');
+    newButton.textContent = 'Block';
+    document.body.appendChild(newButton);
+}
+
+function PeterHouseRun() {
+    window.location.href="https://jasperweb.pages.dev/Padms_in_Javascript/Padms.html";
 }
