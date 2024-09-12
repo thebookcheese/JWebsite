0843b8585a4f68e3c9d4ea2d96f70155e08b370a
thebookcheese <160770461+thebookcheese@users.noreply.github.com>
2024-09-12 11:18:16 +0100

    yfiiyfiyggyi

--- CloaklieClickler/Clicker.js
+++ CloaklieClickler/Clicker.js
@@ -1,9 +1,9 @@
-Cloklies = 0
-ClokliesAdd = 1
-ClickAddCount = 0
-ClickAddCost = 20
-const Button1 = document.querySelector(".Button1")
 
+let Cloklies = 0;
+let ClokliesAdd = 1;
+let ClickAddCount = 0;
+let ClickAddCost = 20;
+const Button1 = document.querySelector(".Button1");
 
 function Click() {
     Cloklies = Cloklies + ClokliesAdd; // Adds CloklieAdd to Cloklies (Increases amount of Cloklies)
@@ -11,18 +11,18 @@ function Click() {
 }
 
 function ClickAdd() {
+    console.log(Button1)
     if (ClickAddCount < 20) {
         if (Cloklies >= ClickAddCost) {
-            Cloklies = Cloklies - ClickAddCost; // Takes away the cost of the upgrade from the amount of currency
-            ClokliesAdd = ClokliesAdd + 1; // Adds the actual increase to currency production on click
-            ClickAddCount = ClickAddCount + 1; // Adds 1 to how many times it has been upgraded
+            Cloklies -= ClickAddCost; // Deducts cost from Cloklies
+            ClokliesAdd += 1; // Increases Cloklies production
+            ClickAddCount += 1; // Tracks upgrade count
 
-            document.getElementById("ClickAddCost").innerHTML = "Cost = ",ClickAddCost;
-            document.getElementById("ClickAddUpgradeCount").innerHTML = ClickAddCount," / 20" 
-            document.getElementById("CloklieCounter").innerHTML = Cloklies
-            // Above code sets the html values
+            document.getElementById("ClickAddCost").innerHTML = "Cost = " + ClickAddCost;
+            document.getElementById("ClickAddUpgradeCount").innerHTML = ClickAddCount + " / 20";
+            document.getElementById("CloklieCounter").innerHTML = Cloklies; // Updates the display
         }
     } else {
-        Button1.style.setProperty("--Button1Visibility", "hidden");
+        Button1.style.setProperty("--Button1Visibility", hidden); // Hides button when max upgrades reached
     }
 }
